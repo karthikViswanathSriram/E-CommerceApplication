603df64b1aa398534f6aa93ea66e95dfbf7721c1
karthikViswanathSriram <ksriram2@asu.edu>
2025-07-28 19:51:30 -0700

    Home component and swiping banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 import Products from './components/product/Products'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home';
 
 function App() {
+  const [count,setCount] = useState();
+
   return (
-    <>
-      <Products/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products' element={<Products/>}/>
+      </Routes>
+    </Router>
   )
 }
 
